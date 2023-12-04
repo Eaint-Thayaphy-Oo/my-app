5600bc04ab999d6cc65781168a6eea7003cf4ab8
Eaint-Thayaphy-Oo <eaintthayaphyoo098@gmail.com>
2023-12-04 16:02:22 +0630

    index.js commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import ReactDom from "react-dom";
 //   return <h1>Hello World</h1>;
 // }
 
-function Greeting(){
-  return(
-    <div>
-      <h1>This is heading</h1>
-      <p>This is paragraph</p>
-    </div>
+function Greeting() {
+  return (
+    <>
+      <div>
+        <h1>This is heading</h1>
+        <p>This is paragraph</p>
+      </div>
+
+      <section>
+        <div>this is heading2</div>
+        <p>loremloremmm</p>
+        <input type="text" />
+        <img src="" alt="title" />
+      </section>
+
+      <React.Fragment>
+        <div>hellooo</div>
+        <div>hi this is testing</div>
+      </React.Fragment>
+    </>
   );
 }
 
